30244504d8a833a6ee56f3d0fe025114bc580f45
kyASse <akyasmuhamadzaidan59@gmail.com>
2025-10-07 22:41:28 +0700

    feat: Enhance PurchaseOrderDetail model and factory with base_price and received_days attributes

--- database/factories/PurchaseOrderDetailFactory.php
+++ database/factories/PurchaseOrderDetailFactory.php
@@ -18,8 +18,12 @@ class PurchaseOrderDetailFactory extends Factory
                 // Use existing item SKU or create one
                 return Item::inRandomOrder()->value('sku') ?? Item::factory()->create()->sku;
             },
+            'base_price' => $this->faker->numberBetween(5000, 50000),
             'quantity' => $this->faker->numberBetween(1, 100),
-            'amount' => $this->faker->randomFloat(2, 10000, 1000000),
+            'amount' => function (array $attributes) {
+                return $attributes['base_price'] * $attributes['quantity'];
+            },
+            'received_days' => $this->faker->numberBetween(0, 30),
         ];
     }
 
